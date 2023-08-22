a06ca97f63c0f1b41b6330d4ad9e9f153b83bb64
Tomas Mayorga <tomasmayorga18@gmail.com>
2023-08-22 15:41:27 -0300

    22

---
+++ src/findOneByEmail.ts
@@ -0,0 +1,11 @@
+import { Contacto, contactos } from "./contactos";
+
+const findOneByEmail = (email: string): Contacto => {
+  function probarSiTieneElMismoEmail(contacto: Contacto) {
+    return contacto.email === email;
+  }
+  const encontrados = contactos.filter(probarSiTieneElMismoEmail);
+  return encontrados[0];
+};
+
+export { findOneByEmail };
